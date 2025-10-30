6692ac7b57736bb0276a11c3c402ea95c0210517
Saad Hussain <sh2384692@gmail.com>
2025-10-30 21:08:41 +0500

    Add API URL console log for debugging

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {Provider} from "react-redux";
-import store from "./component/redux/store.js"
+import store from "./component/redux/store.js";
+
+console.log("API URL:", process.env.REACT_APP_API_URL);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
