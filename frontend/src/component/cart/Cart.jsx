57dd280b6ae98bb57199b1e76ea135fe997499e7
Saad Hussain <sh2384692@gmail.com>
2025-11-03 16:22:00 +0500

    Meta Pixel Added Successfuly

--- frontend/src/component/cart/Cart.jsx
+++ frontend/src/component/cart/Cart.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
 import {removeCartItem, setUserCart} from '../redux/features/cartSlice.js';
 
 const Cart=()=>{
     const {cartItems}=useSelector((state)=>state.cart);
     const dispatch=useDispatch();
+    const navigate=useNavigate();
 
         const increaseQty = (item,quantity) => {
     const newQty=quantity+1;
